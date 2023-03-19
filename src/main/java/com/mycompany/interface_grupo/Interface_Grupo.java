c68d1e8ad848dea6eb25987bf463a67164e1db25
Ricardo Berndt <ricardo.berndt@premiersoft.net>
2023-03-19 15:33:37 -0300

    new project

---
+++ src/main/java/com/mycompany/interface_grupo/Interface_Grupo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interface_grupo;
+
+/**
+ *
+ * @author PremierSoft
+ */
+public class Interface_Grupo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
